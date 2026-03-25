5759b0c95e6f8b4b0b87f02ae534c4b6c758cbb1
Gabriela Nirmal <gabynirmal@icloud.com>
2026-03-25 16:38:35 -0400

    a5

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import cors from "cors";
+const app = express();
+Hello(app);
+app.use(cors());
+app.use(express.json());
+Lab5(app);
+app.listen(process.env.PORT || 4000);
