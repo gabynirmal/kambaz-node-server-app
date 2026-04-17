b8792dcb245aa76fdc56b7a680c8489cb75d800c
Gabriela Nirmal <gabynirmal@icloud.com>
2026-04-17 19:54:04 -0400

    add schema for quizzes

--- kambaz/assignments/schema.js
+++ kambaz/assignments/schema.js
@@ -11,5 +11,35 @@ const schema = new mongoose.Schema({
   assignTo: String,
   assignmentGroup: String,
   description: String,
+  isPublished: { type: Boolean, default: false },
+  shuffleAnswers: Boolean,
+  hasTimeLimit: Boolean,
+  timeLimit: Number,
+  multipleAttempts: Boolean,
+  howManyAttempts: { type: Number, default: 1 },
+  questions: [
+    {
+      _id: String,
+      type: {
+        type: String,
+        enum: ["multiple_choice", "true_false", "fill_in_blank"],
+        default: "multiple_choice",
+      },
+      title: { type: String, default: "New Question" },
+      points: { type: Number, default: 1 },
+      question: { type: String, default: "" },
+      choices: [{ _id: String, text: String }], // for multiple_choice
+      correctAnswers: [String],
+      correctAnswer: { type: String, default: "" },
+    },
+  ],
+  attempts: [
+    {
+      studentId: String,
+      answers: { type: Map, of: String },
+      score: Number,
+      submittedAt: String,
+    },
+  ],
 });
 export default schema;
